8de796c95491395be34bec6aa4c84f87fa67f06a
Matt Stave <mstave@gmail.com>
2018-10-08 07:39:23 -0500

    clean up error handling

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/mstave/golistener
 
-require github.com/gorilla/mux v1.6.2 // indirect
+require (
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/handlers v1.4.0
+	github.com/gorilla/mux v1.6.2
+)
--- listener.go
+++ listener.go
@@ -17,17 +17,23 @@ func logHandler(f http.HandlerFunc) http.HandlerFunc {
 }
 
 func local(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "headers %s \n", r.Header["User-Agent"][0])
+	if _, err := fmt.Fprintf(w, "headers %s \n", r.Header["User-Agent"][0]); err != nil {
+		log.Println(err)
+	}
 }
 
-func remote(w http.ResponseWriter, r *http.Request) {
+func remote(w http.ResponseWriter, _ *http.Request) {
 	remoteURL := "http://127.0.0.1:8082/local"
 	response, err := http.Get(remoteURL)
 	if err != nil {
-		fmt.Fprintf(w, "Error contacting remote at %s, err: %v \n", remoteURL, err)
+		if _, pErr := fmt.Fprintf(w, "Error contacting remote at %s, err: %v \n", remoteURL, err); pErr != nil {
+			log.Println(pErr)
+		}
 	} else {
 		body, _ := ioutil.ReadAll(response.Body)
-		w.Write(body)
+		if _, err := w.Write(body); err != nil {
+			log.Println(err)
+		}
 	}
 }
 
@@ -40,7 +46,6 @@ func serve() {
 	r.HandleFunc("/local", logHandler(local))
 	r.HandleFunc("/remote", logHandler(remote))
 	log.Println("Listening on port", port)
-
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 func main() {
